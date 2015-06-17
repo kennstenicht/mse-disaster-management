cd8f0c628fa1d133686a92049874ee8f576db659
kennstenicht <christoph@Christophs-Air.fritz.box>
2015-06-17 10:40:53 +0200

    add model hence

--- app/models/lence.js
+++ app/models/lence.js
@@ -1,13 +1,12 @@
 import DS from 'ember-data';
-import { Model } from 'ember-pouch';
 
-export default Model.extend({
-  markerId    : DS.attr('number'),
-  title       : DS.attr('string'),
-  isActive    : DS.attr('boolean'),
-  geoPointLat : DS.attr('number'),
-  geoPointLng : DS.attr('number'),
-  pointX      : DS.attr('number'),
-  pointY      : DS.attr('number'),
-  zoom        : DS.attr('number'),
+export default DS.Model.extend({
+  markerId: DS.attr('number'),
+  title: DS.attr('string'),
+  isActive: DS.attr('boolean'),
+  lat: DS.attr('number'),
+  lng: DS.attr('number'),
+  posX: DS.attr('number'),
+  posY: DS.attr('number'),
+  zoom: DS.attr('number')
 });
