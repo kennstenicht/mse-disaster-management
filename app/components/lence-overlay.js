763cc12512108f1055ffc3a96480ead4d5dab423
kennstenicht <christoph@Christophs-Air.fritz.box>
2015-06-19 11:00:43 +0200

    add resize function to lence

--- app/components/lence-overlay.js
+++ app/components/lence-overlay.js
@@ -2,7 +2,11 @@ import Ember from 'ember';
 
 const {
   Component,
-  observer
+  observer,
+  $,
+  run: {
+    bind
+  }
 } = Ember;
 
 export default Component.extend({
@@ -10,9 +14,39 @@ export default Component.extend({
 
   didInsertElement: function() {
     this.setPosition();
+    interact('.lence-overlay')
+      .resizable({
+        edges: { left: false, right: '.lence-overlay__right-handler', bottom: '.lence-overlay__bottom-handler', top: false }
+      })
+      .on('resizemove', bind(this, function (event) {
+        var target = event.target;
+        target.style.width  = event.rect.width + 'px';
+        target.style.height = event.rect.height + 'px';
+
+        this.get('map').resize();
+      }))
+      .on('resizeend', bind(this, function() {
+        this.get('settings')
+          .set('width', this.$().width())
+          .set('height', this.$().height())
+        .save();
+      }));
   },
 
   setPosition: observer('settings.posY', 'settings.posX', function() {
-    this.$().css({'top': this.get('settings.posY'), 'left': this.get('settings.posX')});
-  })
+    this.$().css({
+      'top': this.get('settings.posY'),
+      'left': this.get('settings.posX'),
+      'width': this.get('settings.width'),
+      'height': this.get('settings.height'),
+      'transform': 'rotate('+this.get('settings.angle')+'rad)',
+    });
+    this.get('map').resize();
+  }),
+
+  actions: {
+    setMap: function(map) {
+      this.set('map', map);
+    }
+  }
 });
