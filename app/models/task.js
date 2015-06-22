81bc94fc78cfd7201dfb5dcf37ad67f9be5db78a
Christoph Wiedenmann <info@kennstenicht.de>
2015-06-22 10:11:48 +0200

    edit task model

--- app/models/task.js
+++ app/models/task.js
@@ -7,5 +7,11 @@ export default DS.Model.extend({
   layerType:    DS.attr('string'),
   geoPoints:    DS.attr('array'),
   points:       DS.attr('array'),
-  subtask:      DS.hasMany('subtask')
+  subtask:      DS.hasMany('subtask'),
+  units:        DS.hasMany('unit'),
+  category:     DS.attr('string'),
+  taskName:     DS.attr('string'),
+  status:       DS.attr('string'),
+  progress:     DS.attr('number'),
+  priority:     DS.attr('string')
 });
