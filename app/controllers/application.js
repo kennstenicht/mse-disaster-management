05cd081bf3407bf1d67f918c062e5802287381e2
Christoph Wiedenmann <info@kennstenicht.de>
2015-06-29 15:46:22 +0200

    demo content init function

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -9,22 +9,17 @@ const {
 } = Ember;
 
 export default Controller.extend({
+  // init: function() {
+  //    var model = this.store.createRecord('task-option', {
+  //      title: 'Gebiet Sichern',
+  //    });
+  //    model.save();
+  // },
+
   actions: {
     addLence: function(object) {
       var lence = this.get('maps').filterBy('markerId', object.symbolId).objectAt(0);
       lence.set('active', true).save();
-      // var map = this.store.createRecord('map', {
-      //   title: 'Map '+object.symbolId,
-      //   lat: 51.279023,
-      //   lng: 6.764510,
-      //   posX: 0,
-      //   posY: 0,
-      //   zoom: 13,
-      //   baseMap: true,
-      //   active: true,
-      //   markerId: object.symbolId
-      // });
-      // map.save();
     },
 
     removeLence: function(object) {
