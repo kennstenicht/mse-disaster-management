448c24430172b93db5ae1151e664f50870a7697a
Christoph Wiedenmann <info@kennstenicht.de>
2015-07-06 15:36:31 +0200

    add task options

--- app/models/task-option.js
+++ app/models/task-option.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  string: DS.attr('string')
+  string: DS.attr('string'),
+  string_start: DS.attr('string')
 });
